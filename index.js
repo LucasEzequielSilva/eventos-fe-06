178cc7d99033143c0d9c2cdc8a5519643517eb62
unknown <luxassilva@gmail.com>
2023-03-09 23:19:02 -0300

    fetch implementado

--- index.js
+++ index.js
@@ -1,10 +1,71 @@
-//data
-import data from './data.js'
 //elements
 const $container = document.getElementById('container');
 const fragment = document.createDocumentFragment();
 const $radios = document.getElementById('radios');
 const $search = document.querySelector('input[placeholder="search"]');
+const $reset = document.getElementById('reset');
+const $spinner = document.getElementById('spinner');
+
+let data = []
+let categories = ""
+
+// show spinner
+const showSpinner = () => {
+    $spinner.classList.add('spinner--active');
+  };
+  showSpinner()
+  // hide spinner
+  const hideSpinner = () => {
+    $spinner.classList.remove('spinner--active');
+  };
+  
+async function getData() {
+    try {
+        let apiUrl = "https://planti.onrender.com/api/plantas"
+        fetch(apiUrl)
+            .then(res => res.json()) //leer la información en un formato que entendamos (como json)
+            .then(res => {
+                data = res.data/* igualo mi variable vacia a la respuesta.data (esto porque la respuesta es un objeto si no entienden hagan console.log hasta de su tia) */
+                hideSpinner()
+                createCards(data, $container)
+
+                categories = createCategories(data)
+                createRadios(categories, $radios)
+
+                /* EVENTOS */
+                const filterAndPrint = () => {
+                    let dataFiltered = filterSearch(data, $search.value)
+                    dataFiltered = filterRadios(dataFiltered)
+                    if (dataFiltered.length === 0) {
+                        const $noResults = document.getElementById('no-results')
+                        $noResults.style.display = 'block'
+                    } else {
+                        const $noResults = document.getElementById('no-results')
+                        $noResults.style.display = 'none'
+                    }
+                    createCards(dataFiltered, $container)
+                }
+
+                $radios.addEventListener('change', (e) => {
+                    filterAndPrint();
+                })
+
+                $search.addEventListener('keyup', (e) => {
+                    filterAndPrint();
+                })
+                $reset.addEventListener('click', () => {
+                    document.querySelectorAll('input[type="radio"]:checked').forEach(radio => {
+                        radio.checked = false;
+                    });
+                    filterAndPrint();
+                });
+            })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+    getData()
 
 const createCards = (array, container) => {
     container.innerHTML = ""
@@ -20,56 +81,41 @@ const createCards = (array, container) => {
     container.appendChild(fragment)
 }
 
-createCards(data, $container)
+const createCategories = (array) => {
+    let categories = array.map(category => category.type)
 
-const createCategories = (array) =>{
-    let categories = array.map(category=> category.type)
-
-    categories = categories.reduce((acumulador, elemento)=>{
-        if(!acumulador.includes(elemento)){
+    categories = categories.reduce((acumulador, elemento) => {
+        if (!acumulador.includes(elemento)) {
             acumulador.push(elemento);
         }
         return acumulador
     }, [])
-    return categories 
+    return categories
 }
-let categories = createCategories(data)
+
 const createRadios = (array, container) => {
-    array.forEach( category=>{
+    array.forEach(category => {
         let div = document.createElement('div')
         div.className = `radios-container ${category.toLowerCase()}`
         div.innerHTML = `
-        <input type="radio" id="${category.toLowerCase()}" name="category" />
+        <input type="radio" id="${category.toLowerCase()}" name="category" value="${category.toLowerCase()}" />
         <label for="${category.toLowerCase()}">${category}</label>
         `
         container.appendChild(div)
     })
 }
-createRadios(categories, $radios)
 
 const filterSearch = (array, value) => {
-        let filteredArray = array.filter(element=> element.name.toLowerCase().includes(value.toLowerCase()))
-            return filteredArray
+    let filteredArray = array.filter(element => element.name.toLowerCase().includes(value.toLowerCase()))
+    return filteredArray
 }
+
 const filterRadios = (array) => {
     let checked = document.querySelector('input[type="radio"]:checked');
-    console.log(checked)
-     let filteredArray = array.filter(element => element.type.toLowerCase().includes(checked.id.toLowerCase()))
-     return filteredArray
-}
-
-const filterAndPrint =  (array) =>{
-    let arrayFiltered = filterSearch(array, $search.value)
-    arrayFiltered = filterRadios(arrayFiltered)
-    return arrayFiltered
-}
-
- $search.addEventListener('keyup', (e) =>{
-    let dataFilter = filterAndPrint(data)
-    createCards(dataFilter, $container)
-})
-
-$radios.addEventListener('change', ()=>{
-    let dataFilter = filterAndPrint(data)
-    createCards(dataFilter, $container)
-}) 
+    if (checked) {
+      let filteredArray = array.filter(element => element.type.toLowerCase().includes(checked.value.toLowerCase()))
+      return filteredArray
+    } else {
+      return array;
+    }
+  }
